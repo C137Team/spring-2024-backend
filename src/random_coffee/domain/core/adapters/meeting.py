d1769037dfdbc37d6fee8bd392975926facab1ff
Mihail Shapovalov <mihailsapovalov05@gmail.com>
2024-04-21 14:56:26 +0300

    Fixup

--- src/random_coffee/domain/core/adapters/meeting.py
+++ src/random_coffee/domain/core/adapters/meeting.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from sqlalchemy import select
 
-from random_coffee.domain.core.models import Meeting, Person
+from random_coffee.domain.core.models import Meeting, Person, Employee
 from random_coffee.domain.core.models.meeting_participant import \
     MeetingParticipant
 from random_coffee.domain.core.models.meeting_state import MeetingStateEnum
@@ -13,6 +13,7 @@ class AllMeetings(BaseEntityRepo[Meeting]):
     async def active_for_person(self, person_id: int):
         stmt = (select(Meeting)
                 .join(MeetingParticipant)
+                .join(Employee)
                 .join(Person)
                 .where(Meeting.state.in_((MeetingStateEnum.PLANNED,
                                           MeetingStateEnum.SCHEDULED,
