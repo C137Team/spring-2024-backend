9eba183099bcfd7f79c22b38aecc4a1f294ca743
Mihail Shapovalov <mihailsapovalov05@gmail.com>
2024-04-21 19:10:11 +0300

    And major docline

--- src/random_coffee/application/passthrough/impl.py
+++ src/random_coffee/application/passthrough/impl.py
@@ -8,6 +8,12 @@ from random_coffee.infrastructure.service import BaseService
 
 
 class Passthrough(BaseService):
+    """Passthrough
+
+    This is temporary solution to pass domain object to the presentation
+    layer, due lack of time.
+
+    """
     def __init__(
             self,
             all_meetings: AllMeetings,
